47fa245df75c1363c5ca9433442500c90474dfd6
Notoriousmb10 <yashbhingarde9@gmail.com>
2025-05-16 16:17:50 +0530

    implemented route for all tasks and wrote logic for crud operations

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -38,3 +38,80 @@ const createUser = async (req, res) => {
     res.status(500).json({ msg: "Error creating user" });
   }
 };
+
+const updateUser = async (req, res) => {
+  try {
+    const { userId, newRole } = req.body;
+    const user = await User.findOne({ userId });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    user.role = newRole;
+    await user.save();
+    res.json({
+      msg: "User updated",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Error updating user" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findOneAndDelete({ userId });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    res.json({
+      msg: "User deleted",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Error deleting user" });
+  }
+};
+
+const listUsers = async (req, res) => {
+  try {
+    const theOneWhoMadeTheRequest = req.user;
+    const page = parseInt(req.query.page) || 1;
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
+    let filter = {};
+    if (theOneWhoMadeTheRequest.role === "USER") {
+      return res.status(403).json({ msg: "Unauthorized to list users" });
+    } else if (theOneWhoMadeTheRequest.role === "UNITMANAGER") {
+      filter = {
+        $or: [{ createdBy: theOneWhoMadeTheRequest.userId }, { groupId: theOneWhoMadeTheRequest.groupId }],
+        role: "USER",
+      };
+    } else if (req.user.role === "ADMIN") {
+      filter = {
+        $or: [{ createdBy: theOneWhoMadeTheRequest.userId }, { groupId: theOneWhoMadeTheRequest.groupId }],
+      };
+    } else if (req.user.role === "SUPERADMIN") {
+      filter = {
+        $or: [{ createdBy: theOneWhoMadeTheRequest.userId }, { groupId: theOneWhoMadeTheRequest.groupId }],
+      };
+    }
+    const users = await User.find(filter)
+      .select("-password")
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ users });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Error listing users" });
+  }
+};
+
+module.exports = {
+  createUser,
+  deleteUser,
+  updateUser,
+  listUsers,
+};
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,38 @@
+const express = require("express");
 const { authenticateUserr , authorizeDRoles } = require("../middleware/authMiddle");
-
+import { createUser } from "../controllers/userController";
+import {
+    createUser,
+    updateUser,
+    deleteUser,
+    listUsers
+} from "../controllers/userController";
 router.post(
     '/create',
     authenticateUserr,
     authorizeDRoles("SUPERADMIN", "ADMIN", "UNITMANAGER"),
     createUser
 )
+
+router.patch(
+    "/update-role",
+    authenticateUserr,
+    authorizeDRoles("SUPERADMIN", "ADMIN"),
+    updateUser
+);
+
+router.delete(
+    "/:userId",
+    authenticateUserr,
+    authorizeDRoles("SUPERADMIN", "ADMIN"),
+    deleteUserget
+);
+
+router.get(
+    '/',
+    authenticateUserr,
+    authorizeDRoles("SUPERADMIN", "ADMIN", "UNITMANAGER"),
+    listUsers
+)
+
+module.exports = router;
